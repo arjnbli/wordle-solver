45a3838a96a23338428a00fe228305ececec84b0
Arjun <37790028+arjnbli@users.noreply.github.com>
2022-02-13 11:51:22 -0700

    shift classes to utils.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from utils import Words, WordleRow, WordleCell, KeyBoard
+
 class WordleSolver:
     def __init__(self, target_word, path):
         self.words = Words(path)
@@ -33,14 +35,13 @@ class WordleSolver:
     
     def generate_guess(self):
         pass
-
+    
     def update_row(self, guess):
-        indices = {'correctly_guessed': {}, 'in_word': {}, 'not_in_word': {}}
         for i in range(len(guess)):
             if self.row.cells[i].char != ' ':
                 continue
             if guess[i] == self.target_word[i]:
-                indices['correctly_guessed'][i] = guess[i]
+                self.key_board.correctly_guessed[i] = guess[i]
                 if guess[i] in self.char_frequency:
                     self.update_char_frequency(guess[i])
         
@@ -49,14 +50,14 @@ class WordleSolver:
                 continue
             if guess[i] != self.target_word[i]:
                 if guess[i] in self.char_frequency:
-                    indices['in_word'][i] = guess[i]
+                    self.key_board.in_word[i] = guess[i]
                     self.update_char_frequency(guess[i])
                 else:
-                    indices['not_in_word'][i] = guess[i]
-        print(self.guesses[-1], indices)
-        self.row.update_domains_correctly_guessed(indices['correctly_guessed'])
-        self.row.update_domains_in_word(indices['in_word'])
-        self.row.update_domains_not_in_word(indices['not_in_word'])
+                    self.key_board.not_in_word[i] = guess[i]
+        print(self.guesses[-1], self.key_board)
+        self.row.update_domains_correctly_guessed(self.key_board.correctly_guessed)
+        self.row.update_domains_in_word(self.key_board.in_word)
+        self.row.update_domains_not_in_word(self.key_board.not_in_word)
 
     
     def update_char_frequency(self, char):
@@ -75,87 +76,6 @@ class WordleSolver:
                 hash_table[char] += 1
         return hash_table
 
-class WordleRow:
-    def __init__(self, word_length):
-        self.cells = [WordleCell() for i in range(word_length)]
-    
-    def update_domains_correctly_guessed(self, char_dict):
-        for i in range(len(self.cells)):
-            cell = self.cells[i]
-            if i in char_dict:
-                cell.domain = set([char_dict[i]])
-                continue
-            for char in set(char_dict.values()):
-                if char in cell.domain:
-                    cell.domain.remove(char)
-
-    def update_domains_in_word(self, char_dict):
-        for i in range(len(self.cells)):
-            cell = self.cells[i]
-            if i in char_dict and char_dict[i] in cell.domain:
-                cell.domain.remove(char_dict[i])
-
-    def update_domains_not_in_word(self, char_dict):
-        for char in char_dict.values():
-            for cell in self.cells:
-                if char in cell.domain:
-                    cell.domain.remove(char)
-
-    def __repr__(self):
-        row_vals = []
-        for cell in self.cells:
-            row_vals.append(cell.char)
-        return str(row_vals)
-
-class WordleCell:
-    def __init__(self, char=' '):
-        self.char = char
-        self.domain = set('abcdefghijklmnopqrstuvwxyz')
-    
-    def remove_from_domain(self, char):
-        if char in self.domain:
-            self.domain.remove(char)
-    
-    def __repr__(self):
-        return self.char
-
-class KeyBoard:
-    def __init__(self):
-        self.in_word = set()
-        self.not_in_word = set()
-
-class Words:
-    def __init__(self, path):
-        self.words_set = self.get_valid_words(path)
-        self.words_dict = self.get_words_dict()
-        
-    def get_valid_words(self, path):
-        valid_chars = set('abcdefghijklmnopqrstuvwxyz')
-        with open(path, 'r') as file:
-            valid_words = set()
-            for line in file:
-                word = line.lower().strip()
-                is_valid = True
-                for char in word:
-                    if char not in valid_chars:
-                        is_valid = False
-                        break
-                if is_valid:
-                    valid_words.add(word)
-        return valid_words
-    
-    def get_words_dict(self):
-        words_dict = {}
-        words_list = list(self.words_set)
-        for word in words_list:
-            word_length = len(word)
-            if word_length not in words_dict:
-                words_dict[word_length] = [word]
-            else:
-                words_dict[word_length].append(word)
-        return words_dict
-                
-
 if __name__ == '__main__':
     #target_word = input('Please provide a word: ').lower()
     target_word = 'scary'
@@ -163,4 +83,4 @@ if __name__ == '__main__':
     print(target_word)
     solver.solve()
     print(solver.guesses)
-  
+    # print(solver.generate_guess())
