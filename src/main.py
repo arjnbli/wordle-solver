82d09c5b95fc616893c7ec2c0619a36330e652bb
Arjun <37790028+arjnbli@users.noreply.github.com>
2022-02-13 21:38:32 -0700

    modify main to get solver metrics averaged for 10 passes

--- src/main.py
+++ src/main.py
@@ -12,19 +12,30 @@ def main():
     with open('./data/wordle-all-allowed-words.txt', 'w+') as file:
         for word in wordle_allowed.words_set:
             file.write(word + '\n')
-    
+
     num_solved = 0
     num_guesses = 0
-    for word in wordle_answers.words_set:
+    num_words = len(wordle_answers.words_set)
+    for i in range(10):
+        num_solved_current_pass, num_guesses_current_pass = get_metrics(wordle_answers.words_set)
+        num_solved += num_solved_current_pass
+        num_guesses += num_guesses_current_pass
+    average_guesses = num_guesses / num_solved
+    percentage_solved = num_solved / (num_words*10)
+    print('{} solved out of {}({}%). Average Guesses = {}'.format(num_solved, (num_words*10), percentage_solved, average_guesses))
+
+
+
+def get_metrics(words_set):
+    num_solved = 0
+    num_guesses = 0
+    for word in words_set:
         solver = WordleSolver('./data/wordle-all-allowed-words.txt', word)
         result = solver.solve()
         if result != -1:
             num_solved += 1
             num_guesses += solver.num_guesses
-    average_guesses = num_guesses / num_solved
-    num_words = len(wordle_answers.words_set)
-    percentage_solved = num_solved / num_words
-    print('{} solved out of {}({}%). Average Guesses = {}'.format(num_solved, num_words, percentage_solved, average_guesses))
+    return num_solved, num_guesses
 
 if __name__ == '__main__':
     main()
